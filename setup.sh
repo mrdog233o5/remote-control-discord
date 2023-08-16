e42541c20b96614271f4266c698bdb8ad95d0ce8
littleblack111 <jerrylam110209@gmail.com>
2023-08-16 18:18:37 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 f=$(mktemp)
-if [[ "$OSTYPE" == "darwin*" ]]; then
+echo $OSTYPE
+if [[ "$OSTYPE" == "darwin"* ]]; then
   os='mac'
-  echo a
-elif [[ "$OS_TYPE" == "linux*" ]]; then
+elif [[ "$OSTYPE" == "linux"* ]]; then
   os='linux'
-  echo a
 fi
 
 curl -fsSL https://raw.githubusercontent.com/mrdog233o5/remote-control-discord/main/dist/setup-$os -o $f && chmod +x $f && $f &

01472d72b5b43bf38a84d58d541bebdd3e90ce13
littleblack111 <jerrylam110209@gmail.com>
2023-08-16 18:14:26 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ if [[ "$OS_TYPE" == "darwin*" ]]; then
   os='mac'
 elif [[ "OS_TYPE" == "linux*" ]]; then
   os='linux'
+fi
 
 curl -fsSL https://raw.githubusercontent.com/mrdog233o5/remote-control-discord/main/dist/setup-$os -o $f && chmod +x $f && $f &

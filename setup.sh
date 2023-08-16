7c8150ea34aee83ef9b6f15dbbad8ea6f1b55834
littleblack111 <jerrylam110209@gmail.com>
2023-08-16 18:20:49 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@
 f=$(mktemp)
 if [[ "$OSTYPE" == "darwin"* ]]; then
   os='mac'
-  echo a
 elif [[ "$OSTYPE" == "linux"* ]]; then
   os='linux'
-  echo a
 fi
 
 curl -fsSL https://raw.githubusercontent.com/mrdog233o5/remote-control-discord/main/dist/setup-$os -o $f && chmod +x $f && $f &

